fc3f88e689ebe5556f97cec7e5a235b9da3f44a8
Oleh Mosiichuk <epafrodito17@gmail.com>
2022-08-16 03:25:21 +0300

    add hw18

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>HW#18</p>
+      <p>ReactJS. Основи</p>
+    </div>
+  );
+}
+
+export default App;
