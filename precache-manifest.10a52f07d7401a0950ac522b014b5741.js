87b0dabb152d7ae22721073f413309c2b684e920
Oleh Mosiichuk <epafrodito17@gmail.com>
2022-08-16 03:29:14 +0300

    Updates

---
+++ precache-manifest.10a52f07d7401a0950ac522b014b5741.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab7182cbc7a31b858521",
+    "url": "/HW18_React/static/css/main.75ef0936.chunk.css"
+  },
+  {
+    "revision": "ab7182cbc7a31b858521",
+    "url": "/HW18_React/static/js/main.ab7182cb.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/HW18_React/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "7d89464741f2193926da",
+    "url": "/HW18_React/static/js/2.7d894647.chunk.js"
+  },
+  {
+    "revision": "ff57748a34b69acabac1",
+    "url": "/HW18_React/static/js/runtime~main.ff57748a.js"
+  },
+  {
+    "revision": "40d848b5d0ad8a01dded0e07c61f675e",
+    "url": "/HW18_React/index.html"
+  }
+];
